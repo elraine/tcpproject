d2071022ac713fcddeeec6d4735b57eed0bf2dbd
achan <laevatinn@gmail.com>
2016-03-16 13:30:58 +0100

    love that client

--- src/Client.java
+++ src/Client.java
@@ -7,10 +7,16 @@ import java.net.InetAddress;
 public class Client implements Runnable{
 String inetcfg;
 int port;
+String msg = "i";
 
 public Client(String inetcfg, int port){
   this.inetcfg = inetcfg;
   this.port = port;
+}
+public Client(String inetcfg, int port, String msg){
+  this.inetcfg = inetcfg;
+  this.port = port;
+  this.msg = msg;
 }
   public void run(){
     InetAddress iadr = null;
@@ -48,6 +54,7 @@ public Client(String inetcfg, int port){
     }
     //putting Strings
     String str = "";
+    pred.println(msg);
     while(true){
       str = sc.nextLine();//getInput
       if(str.equals("/q")) break;
@@ -70,31 +77,53 @@ public Client(String inetcfg, int port){
     String inetcfg = "localhost";
     int port = 8080;
 
-    if(args.length <= 0 ){
-      System.out.println("give args : 0 if reading peers from file, 1 if giving args now. %n If 0 : follow the 0 with name of file. %n If 1 : 1 <ipadress> <port>");
-    }else if(Integer.parseInt(args[0]) == 0 && args.length < 3){
-//reads from file TODO
-      filename = args[1];
-      System.out.println("not implemented yet file name is "+filename);
-    }else if(Integer.parseInt(args[0]) == 1 && args.length < 4){
-    inetcfg = args[1];
-    Integer.parseInt(args[2]);
-  }else{
-    System.out.println("not good");
-    // System.exit();
-  }
-    Client obj = new Client(inetcfg, port);
+//     if(args.length <= 0 ){
+//       System.out.println("give args : 0 if reading peers from file, 1 if giving args now. %n If 0 : follow the 0 with name of file. %n If 1 : 1 <ipadress> <port>");
+//     }else if(Integer.parseInt(args[0]) == 0 && args.length < 3){
+// //reads from file TODO
+//       filename = args[1];
+//       System.out.println("not implemented yet file name is "+filename);
+//     }else if(Integer.parseInt(args[0]) == 1 && args.length < 4){
+//     inetcfg = args[1];
+//     Integer.parseInt(args[2]);
+//   }else{
+//     System.out.println("not good");
+//     // System.exit();
+//   }
+
+    //Single thread test
+    //\\\\\\\\\\\\\
+    // Client obj = new Client(inetcfg, port);
+    // Thread tobj = new Thread(obj);
+    // tobj.start();
+
     //Thread test
+    //\\\\\\\\\\\\\
     // int baseport = 8080;
-    // int maxthr = 3;
-    // Client[] object;
-    // Thread thobj;
-    // for (int i = 0;i < 3 ; i++) {
-    //   object[i] = new Client("localhost", baseport +i);
-    //
+    // int maxnb = 3;
+    // Client[] object = new Client[maxnb];
+    // Thread[] thobj = new Thread[maxnb];
+    // for (int i = 0; i < maxnb ; i++) {
+    //   System.out.println(baseport +i);
+    //   object[i] = new Client("localhost", (baseport +i),"i am number "+i);
+    //   thobj[i] = new Thread(object[i]);
+    //   thobj[i].start();
+    //   Thread.sleep(1000);
     // }
 
-    Thread tobj = new Thread(obj);
-    tobj.start();
+    Client object0 = new Client("localhost", 8080, "i am 0");
+    Client object1 = new Client("localhost", 8080, "i am 1");
+    Client object2 = new Client("localhost", 8080, "i am 2");
+    Thread to0 = new Thread(object0);
+    Thread to1 = new Thread(object1);
+    Thread to2 = new Thread(object2);
+    to0.start();
+    to1.start();
+    to2.start();
+
+
+    // Thread tobj = new Thread(obj);
+    // tobj.start();
     }
+
   }
