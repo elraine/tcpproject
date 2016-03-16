b293fbf8db25ef6a74901b648fc7fd202e833264
GANGLOFF Nolwenn <nolwenn.gangloff@gmail.com>
2016-03-16 08:59:18 +0100

    suite Tracker

--- src/Tracker.c
+++ src/Tracker.c
@@ -10,8 +10,22 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <arpa/inet.h>
 #include "Seeded_file.h"
 #include "Seeder.h"
+
+typedef struct{
+	int portno;	
+	int sockfd;
+	struct sockaddr_in addr;
+	socklen_t clilen;
+
+	char *seeder_IP;
+}seeder;
+
+
+
 
 void error(const char *msg) {
     perror(msg);
@@ -25,6 +39,26 @@ typedef struct{
 	
 }tracker;
 
+void *connection_handler(void *sockfd){
+	
+	seeder seed;
+	seed.sockfd = *(int*)sockfd;
+	
+	char buffer[2000];
+	int nb_read;
+	
+	char* essai = "coucou me voila";
+	write(seed->sockfd , essai , strlen(essai));
+	
+	while( (nb_read = recv(seed.sockfd, client_message , 2000 , 0)) > 0 ){
+		//TODO read
+	}
+	
+	if(nb_read==0){
+		//connection terminée
+	}
+	return 0;
+}
 
 /*
 void parse_message(char* mess){
@@ -56,6 +90,7 @@ void tracker_init(tracker *t, int portno){
 	t->sockfd = socket(AF_INET, SOCK_STREAM, 0);	
 	if (t->sockfd < 0)
 		error("ERROR opening socket");
+	puts("Socket created");
 	
 	bzero((char *) &(t->addr), sizeof(t->addr));
 	t->addr.sin_family = AF_INET;
@@ -64,6 +99,9 @@ void tracker_init(tracker *t, int portno){
     
     if (bind(t->sockfd, (struct sockaddr *) &(t->addr), sizeof(t->addr)) < 0)
         error("ERROR on binding");
+	
+	puts("Binding done");
+
 }
 
 void usage (char *s){
@@ -74,14 +112,42 @@ void usage (char *s){
 #define param 1
 int main(int argc, char *argv[]){
 	
+	int c, seeder_sockfd;
+	struct sockaddr_in seeder_addr;
+	
 	if (argc != param+1) 
 		usage(argv[0]);
  		
 	tracker track;
 	tracker_init(&track,atoi(argv[1]));
 	
-	listen(track->sockfd, 5);
-
+	listen(track.sockfd, 5);
+	
+    c = sizeof(struct sockaddr_in);
+	pthread_t thread_id;
+	
+	while( (seeder_sockfd = accept(track.sockfd, (struct sockaddr *)&seeder_addr, (socklen_t*)&c)) ){
+		
+		puts("Connection accepted");
+         
+        if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &seeder_sockfd) < 0)
+        {
+            error("ERROR on creating thread");
+            return 1;
+        }		
+		
+        //pthread_join( thread_id , NULL);
+        puts("Handler assigned");
+    }
+     
+    if (seeder_sockfd < 0)
+    {
+        error("ERROR on accpeting connection");
+        return 1;
+    }
+     
+    return 0;
+}
 	
     /*
     char buffer[256];
@@ -109,5 +175,4 @@ int main(int argc, char *argv[]){
     close(newsockfd);
     close(sockfd);*/
     
-    return 0;
-}
+
