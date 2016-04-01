07edba540fcc2ef5aa09f56a8c072de4138c9499
elraine <laevatinn@gmail.com>
2016-04-01 10:39:13 +0200

    qwe

--- src/Client.java
+++ src/Client.java
@@ -6,9 +6,13 @@ import java.net.InetAddress;
 
 public class Client implements Runnable{
   //static final int port = 8080;
-static String inetcfg;
-static int port;
+  String inetcfg;
+  int port;
 
+  Client(String inetcfg, int port){
+    this.inetcfg = inetcfg;
+    this.port = port;
+  }
   public void run(){
     InetAddress iadr = null;
     Socket socket = null;
@@ -44,18 +48,21 @@ static int port;
       ie.printStackTrace();
     }
     //putting Strings
-    String str = "";
-    while(true){
-      str = sc.nextLine();//getInput
-      if(str.equals("/q")) break;
+
+    String str = "Hello this is Client from " + port;
+    int i = 0;
+    while(i < 10){
+      //str = sc.nextLine();//getInput
       pred.println(str); //send input to serv
+      if(str.equals("/q")) break;
+      i++;
     }
     System.out.println("/q");
     pred.println("/q");
     try{
-    plec.close();
-    pred.close();
-    socket.close();
+      plec.close();
+      pred.close();
+      socket.close();
     }catch(IOException ie){
       ie.printStackTrace();
     }
@@ -64,13 +71,21 @@ static int port;
 
   public static void main(String[] args) throws Exception{
     if(args.length < 2 ){
-      System.out.println("GIVE ARGUMENTS MAGGOT");
+      System.out.println("usage : IPAdress port");
     }else{
-    inetcfg = args[0];
-    port = Integer.parseInt(args[1]);
-    Client obj = new Client();
-    Thread tobj = new Thread(obj);
-    tobj.start();
+      /*
+      inetcfg = args[0];
+      port = Integer.parseInt(args[1]);
+      */
+      Client obj = new Client("localhost", 8080);
+      Client obj1 = new Client("localhost", 8081);
+      Client obj2 = new Client("localhost", 8082);
+      Thread tobj = new Thread(obj);
+      Thread tobj1 = new Thread(obj1);
+      Thread tobj2 = new Thread(obj2);
+      tobj.start();
+      tobj1.start();
+      tobj2.start();
     }
   }
 }
