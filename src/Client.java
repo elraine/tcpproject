a7a2d2e76ca49ec6c91a8a3c998140ceab0ac1bc
elraine <laevatinn@gmail.com>
2016-04-15 17:31:27 +0200

    ll

--- src/Client.java
+++ src/Client.java
@@ -38,8 +38,8 @@ public class Client implements Runnable{
 
     for(File file : fileList){
       //if(file.isFile()){
-        files[i] = new Fichier(file.getName(), file.length());
-        i++;
+      files[i] = new Fichier(file.getName(), file.length());
+      i++;
       //}
     }
   }
@@ -104,40 +104,39 @@ public class Client implements Runnable{
 
     //ReadingInfos
     try{
-    try{
-      plec = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    }catch(IOException ie){
-      ie.printStackTrace();
-    }
-    //Outputting Info
-    try{
-      pred = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-    }catch(IOException ie){
-      ie.printStackTrace();
-    }
-    //putting Strings
-    String str = "";
-    pred.println(msg);
-    while(true){
-      str = sc.nextLine();//getInput
-      if(str.equals("/q")) break;
-      pred.println(str); //send input to serv
-      if(str.equals("/q")) break;
-      i++;
-    }
-    System.out.println("/q");
-    pred.println("/q");
-    try{
-      plec.close();
-      pred.close();
-      socket.close();
-    }catch(IOException ie){
-      ie.printStackTrace();
-    }
+      try{
+        plec = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      }catch(IOException ie){
+        ie.printStackTrace();
+      }
+      //Outputting Info
+      try{
+        pred = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
+      }catch(IOException ie){
+        ie.printStackTrace();
+      }
+      //putting Strings
+      String str = "";
+      pred.println(msg);
+      while(true){
+        str = sc.nextLine();//getInput
+        if(str.equals("/q")) break;
+        pred.println(str); //send input to serv
+        if(str.equals("/q")) break;
+      }
+      System.out.println("/q");
+      pred.println("/q");
+      try{
+        plec.close();
+        pred.close();
+        socket.close();
+      }catch(IOException ie){
+        ie.printStackTrace();
+      }
 
-  }catch(Exception e){
+    }catch(Exception e){
       System.out.println("No socket : "+e);
-}
+    }
   }
   public static void main(String[] args) throws Exception{
     String filename = "localhost";
@@ -145,19 +144,19 @@ public class Client implements Runnable{
     String inetcfg = "localhost";
     int port = 8080;
 
-//     if(args.length <= 0 ){
-//       System.out.println("give args : 0 if reading peers from file, 1 if giving args now. %n If 0 : follow the 0 with name of file. %n If 1 : 1 <ipadress> <port>");
-//     }else if(Integer.parseInt(args[0]) == 0 && args.length < 3){
-// //reads from file TODO
-//       filename = args[1];
-//       System.out.println("not implemented yet file name is "+filename);
-//     }else if(Integer.parseInt(args[0]) == 1 && args.length < 4){
-//     inetcfg = args[1];
-//     Integer.parseInt(args[2]);
-//   }else{
-//     System.out.println("not good");
-//     // System.exit();
-//   }
+    //     if(args.length <= 0 ){
+    //       System.out.println("give args : 0 if reading peers from file, 1 if giving args now. %n If 0 : follow the 0 with name of file. %n If 1 : 1 <ipadress> <port>");
+    //     }else if(Integer.parseInt(args[0]) == 0 && args.length < 3){
+    // //reads from file TODO
+    //       filename = args[1];
+    //       System.out.println("not implemented yet file name is "+filename);
+    //     }else if(Integer.parseInt(args[0]) == 1 && args.length < 4){
+    //     inetcfg = args[1];
+    //     Integer.parseInt(args[2]);
+    //   }else{
+    //     System.out.println("not good");
+    //     // System.exit();
+    //   }
 
     //Single thread test
     //\\\\\\\\\\\\\
@@ -187,27 +186,29 @@ public class Client implements Runnable{
       inetcfg = args[0];
       port = Integer.parseInt(args[1]);
       */
-    Client object1 = new Client("localhost", 8081, "i am 1");
-    Client object2 = new Client("localhost", 8082, "i am 2");
+      Client object1 = new Client("localhost", 8081, "i am 1");
+      Client object2 = new Client("localhost", 8082, "i am 2");
 
-    try{
-      object0.configInit();
+      try{
+        object0.configInit();
+      }
+      catch(FileNotFoundException f){
+        System.out.println("File not found");
+      }
+      object0.announceTracker();
+
+      Thread to0 = new Thread(object0);
+      Thread to1 = new Thread(object1);
+      Thread to2 = new Thread(object2);
+      to0.start();
+      to1.start();
+      to2.start();
+
+
+      // Thread tobj = new Thread(obj);
+      // tobj.start();
     }
-    catch(FileNotFoundException f){
-      System.out.println("File not found");
-    }
-    object0.announceTracker();
 
-    Thread to0 = new Thread(object0);
-    Thread to1 = new Thread(object1);
-    Thread to2 = new Thread(object2);
-    to0.start();
-    to1.start();
-    to2.start();
-
-
-    // Thread tobj = new Thread(obj);
-    // tobj.start();
   }
 
 }
